0bb7f370783c04ec02b09634b697820d99322984
Kemuel Batista <kemuellima20@gmail.com>
2023-10-30 10:12:45 -0300

    feat: adding answers repository dependency for answer use case

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,11 +1,18 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { describe, it, expect } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repositories/answer-repository'
+import { Answer } from '../entities/answer'
+
+const fakeAnswerRepository: AnswerRepository = {
+  create: async (answer: Answer) => {},
+}
 
 describe('Answer Question Use Case', () => {
-  it('should be able to create an answer', () => {
-    const answerQuestion = new AnswerQuestionUseCase()
+  it('should be able to create an answer', async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
-    const answer = answerQuestion.execute({
+    const answer = await answerQuestion.execute({
       instructorId: '1',
       questionId: '1',
       content: 'Nova resposta',
