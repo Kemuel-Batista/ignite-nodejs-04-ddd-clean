267d16e22339838a76588e3832346b3abbed4aaa
Kemuel Batista <kemuellima20@gmail.com>
2023-11-10 15:39:15 -0300

    refactor: adjust on import generic errors

--- src/domain/forum/application/use-cases/answer/choose-question-best-answer.spec.ts
+++ src/domain/forum/application/use-cases/answer/choose-question-best-answer.spec.ts
@@ -4,7 +4,7 @@ import { InMemoryAnswerAttachmentsRepository } from '../../../../../../test/repo
 import { InMemoryAnswersRepository } from '../../../../../../test/repositories/in-memory-answers-repository'
 import { InMemoryQuestionAttachmentsRepository } from '../../../../../../test/repositories/in-memory-question-attachments-repository'
 import { InMemoryQuestionsRepository } from '../../../../../../test/repositories/in-memory-questions-repository'
-import { NotAllowedError } from '../errors/not-allowed-error'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 import { ChooseQuestionBestAnswerUseCase } from './choose-question-best-answer'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
